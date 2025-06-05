64ad972c0c18de08e5ed51bda3b8accabba3c629
Tamires Talier <96202019+tamirestalier@users.noreply.github.com>
2025-06-05 11:50:45 -0300

    arrumeioScrpitBootstrap

--- views/educacao.php
+++ views/educacao.php
@@ -225,6 +225,7 @@
     </main>
                                     
     <?php require_once "global/footer.php"; require_once "global/scripts.php"; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
